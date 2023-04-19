91483aebc467d166801669365536b5e65163e752
gimploo <41667550+gimploo@users.noreply.github.com>
2023-04-19 18:04:17 +0530

    ...

--- frontend/src/pages/Seller/pages/Trends/Trends.jsx
+++ frontend/src/pages/Seller/pages/Trends/Trends.jsx
@@ -1,24 +1,7 @@
-import React from "react";
+import React, {useEffect} from "react";
 import ReactDOM from "react-dom";
 import Script from "react-load-script";
 
-export default function Trends({ type, keyword, time, geo}) {
-  const handleScriptLoad = () => {
-    const property = "froogle";
-    window.trends.embed.renderExploreWidgetTo(
-      document.getElementById("widget"),
-      type,
-      {
-        comparisonItem: [{ keyword:keyword, geo: geo, time: time}],
-        category: 0,
-        property: property
-      },
-      {
-        //"date=now%201-d&geo=IN&gprop=froogle&q=electronics&hl=en-US"
-        exploreQuery: `date=${time}&geo=${geo}&gprop=${property}&q=${encodeURI(keyword)}&hl=en-US`,
-        guestPath: "https://trends.google.com:443/trends/embed/"
-      }
-    );
-  };
+export default function Trends({ type, keyword, time, geo, handleScriptLoad}) {
     return <Script url="https://ssl.gstatic.com/trends_nrtr/3316_RC01/embed_loader.js" onLoad={handleScriptLoad} />;
 }
--- frontend/src/pages/Seller/pages/home/Home.jsx
+++ frontend/src/pages/Seller/pages/home/Home.jsx
@@ -7,17 +7,41 @@ import WidgetLg from "../../components/widgetLg/WidgetLg";
 import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "pages/Seller/components/sidebar/Sidebar";
 import SeasonalChart from "pages/Seller/components/SeasonalChart/Chart"
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import BackendContext from "context/BackendContext";
 import { useNavigate } from "react-router-dom";
 import Trends from "../Trends/Trends";
-import TestTrends from "../Trends/TestTrends";
 
 const About = () => {
-  const type="GEO_MAP";
-  const keyword = "electronics";
+  const type = "GEO_MAP";
+  //const keyword = "electronics";
+  const [keyword, setKeyword] = useState('electronics')
   const time = "now 1-d";
   const geo = "IN";
+  const handleScriptLoad = () => {
+    const property = "froogle";
+    window.trends.embed.renderExploreWidgetTo(
+      document.getElementById("widget"),
+      type,
+      {
+        comparisonItem: [{ keyword:keyword, geo: geo, time: time}],
+        category: 0,
+        property: property
+      },
+      {
+        //"date=now%201-d&geo=IN&gprop=froogle&q=electronics&hl=en-US"
+        exploreQuery: `date=${time}&geo=${geo}&gprop=${property}&q=${encodeURI(keyword)}&hl=en-US`,
+        guestPath: "https://trends.google.com:443/trends/embed/"
+      }
+    );
+  };
+  useEffect(() => {
+    alert("trends "+keyword)
+    document.getElementById("widget").innerHTML = "";
+    handleScriptLoad();
+  }, [keyword])
+  
+
   return (
     <div className='md:flex md:justify-center md:align-center md:mx-4'>
       <div className='container'>
@@ -35,19 +59,25 @@ const About = () => {
           <WidgetSm/>
           <WidgetLg/>
         </div> */}
-          <TestTrends/>
-          <div id="widget">
-            <Trends 
+        <select value={keyword} onChange={(e) => { setKeyword(e.target.value) }}>
+          <option value=''>Select A Category</option>
+          <option value="Electronics">Electronics</option>
+          <option value="Furniture">Furniture</option>
+          <option value="Clothing">Clothing</option>
+        </select>
+        <div id="widget">
+          <Trends
             type={type}
             keyword={keyword}
             time={time}
             geo={geo}
-            />
-          </div>
+            handleScriptLoad={handleScriptLoad}
+          />
         </div>
       </div>
-      
     </div>
+      
+    </div >
   );
 }
 
@@ -55,16 +85,16 @@ const About = () => {
 export default function SellerDashboard() {
 
   const context = useContext(BackendContext)
-  const {isSeller} = context
-  alert("Dashboard:"+isSeller)
-  const navigate = useNavigate()
-  useEffect(() => {
-    if(!isSeller){
-      alert("Unauthorized access")
-      navigate("/product")
-    }    
-  }, [])
-  
+  const { isSeller } = context
+  // alert("Dashboard:" + isSeller)
+  // const navigate = useNavigate()
+  // useEffect(() => {
+  //   if (!isSeller) {
+  //     alert("Unauthorized access")
+  //     navigate("/product")
+  //   }
+  // }, [])
+
   return (
     <>
       {/* <Topbar /> */}
