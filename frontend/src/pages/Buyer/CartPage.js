75b9f9e00a56ef29c0f5625a4491c0a2c7be0473
AN0NIT <64950553+AN0NIT@users.noreply.github.com>
2023-05-11 23:24:40 +0530

    More edits required to CartPage

--- frontend/src/pages/Buyer/CartPage.js
+++ frontend/src/pages/Buyer/CartPage.js
@@ -3,7 +3,7 @@ import { Dialog, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
 import { Link } from 'react-router-dom'
 import BackendContext from "../../context/BackendContext";
-
+import axios from 'axios'
 
 
 const products = [
@@ -32,24 +32,28 @@ const products = [
   // More products...
 ]
 
-const handleDelete = async (index) => {
-  console.log('index:',index)
-  // axios.get(`${API_SERVER_URL}/addtocart/delete/${order_id}`)
-  //   .then((res) => {
-  //     if (res.status = 200)
-  //       Cart()
-  //     else
-  //       alert("handle delete error")
 
-  //   })
-};
 
 
 
 
 
 function CartItem({ product, index }) {
-  const { MEDIA_SERVER_URL } = useContext(BackendContext)
+  const { MEDIA_SERVER_URL, cartDetails, API_SERVER_URL } = useContext(BackendContext)
+  const handleDelete = async (index) => {
+    // console.log('index:',index)
+    // console.log(cartDetails[index].name)
+    axios.get(`${API_SERVER_URL}/addtocart/delete/${cartDetails[index].id}`)
+      .then((res) => {
+        if (res.status = 200){
+          cartDetails.splice(index,1)
+          console.log('new cartdetaisl:',cartDetails)
+        }
+        else
+          alert("handle delete error")
+  
+      })
+  };
   return (
     <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
       <div class="flex w-2/5">
@@ -122,7 +126,7 @@ export default function Cart() {
             <button class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Checkout</button>
           </div>
 
-          <Link to='/' class="flex font-semibold text-indigo-600 text-sm mt-10">
+          <Link to='/product' class="flex font-semibold text-indigo-600 text-sm mt-10">
             <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
             Continue Shopping
           </Link>
@@ -132,7 +136,7 @@ export default function Cart() {
         <div id="summary" class="hidden md:block w-1/4 px-8 py-10">
           <h1 class="font-semibold text-2xl border-b pb-8">Order Summary</h1>
           <div class="flex justify-between mt-10 mb-5">
-            <span class="font-semibold text-sm uppercase">Items {products.length}</span>
+            <span class="font-semibold text-sm uppercase">Items {products.length+1}</span>
             <span class="font-semibold text-sm">₹{totalPrice}</span>
           </div>
           <div>
